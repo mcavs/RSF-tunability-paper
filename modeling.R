243a647c2dccd5aad2512bf3033462b59cb242bc
Yiğitcan YARDIMCI <147248981+Yigitcanyardimci@users.noreply.github.com>
2025-03-19 23:13:51 +0300

    Update modeling.R

--- modeling.R
+++ modeling.R
@@ -1,11 +1,24 @@
-train_models <- function(task, split) {
+train_rsf_model <- function(task, mtry, ntree, nodesize, splitrule) {
   
-  # Random Survival Forest (RSF) Model ######################################################
+  cv_rsf_10fold <- rsmp("cv", folds = 10)
+  cv_rsf_10fold$instantiate(task)
+
+  rsf_model <- lrn("surv.rfsrc", id = "rfsrc",
+                   ntree = ntree,
+                   mtry = mtry,
+                   nodesize = nodesize,
+                   splitrule = splitrule)
+
+  cv_results <- resample(task, rsf_model, cv_rsf_10fold)
+
+  cv_summary <- cv_results$aggregate(msr("surv.cindex"))  # aggregate ile birlikte 
+                                                          # ortalama C-Index 
+                                                          # değerini alıyoruz
   
-  rfsrc_model <- lrn("surv.rfsrc", id = "rfsrc")
-  rfsrc_model$train(task, row_ids = split$train)
+  cat("C-Index: ", round(cv_summary[["surv.cindex"]], 4), "\n")
   
   return(list(
-    rfsrc_model = rfsrc_model
+    model = rsf_model,
+    c_index = cv_summary[["surv.cindex"]]
   ))
 }
