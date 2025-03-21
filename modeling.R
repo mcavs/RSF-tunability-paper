e2b2039810cd3d7f950970f3fc76c7e204455272
Mustafa CAVUS <45355486+mcavs@users.noreply.github.com>
2025-03-21 21:46:20 +0300

    Update modeling.R

--- modeling.R
+++ modeling.R
@@ -1,6 +1,6 @@
 train_rsf_model <- function(task, hyp) {
   
-  cv_rsf_10fold <- rsmp("cv", folds = 10)
+  cv_rsf_10fold <- rsmp("cv", folds = 5)
   cv_rsf_10fold$instantiate(task)
 
   rsf_model <- lrn("surv.rfsrc", id = "rfsrc",
