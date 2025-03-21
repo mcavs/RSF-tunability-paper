203f419f1e6dcf8756f91f3920e85c6ef56d8418
Mustafa CAVUS <45355486+mcavs@users.noreply.github.com>
2025-03-21 21:45:02 +0300

    Add files via upload

--- run.R
+++ run.R
@@ -1,8 +1,8 @@
 source("install_packages.R")
 source("import_datasets.R")
 source("task.R")
-source("data_split.R")
 source("modeling.R")
+source("train_rsf_subset.R")
 
 task_FD001 <- make_task(data      = train_FD001,
                         censor    = 250,
@@ -17,31 +17,5 @@ task_FD001 <- make_task(data      = train_FD001,
                                       "sensor_19",
                                       "sensor_5"))
 
-hyp <- expand.grid(
-  ntree     = seq(100, 2000, 200),
-  mtry      = seq(1:10),
-  nodesize  = seq(5, 100, 10),
-  nodedepth = seq(5, 100, 10),
-  nsplit    = seq(5, 15, 1),
-  splitrule = c("logrank", "logrankscore", "bs.gradient"))
+# örnek: train_rsf_subset(task_FD001, 1, 10)
 
-start <- Sys.time()
-res <- data.frame() 
-for(i in 1:dim(hyp)[1]){
-  perf <- train_rsf_model(task_FD001, hyp[i, ])
-  
-  new_row <- data.frame(data      = "FD001",
-                        ntree     = hyp[i, ]$ntree,
-                        mtry      = hyp[i, ]$mtry,
-                        nodesize  = hyp[i, ]$nodesize,
-                        nodedepth = hyp[i, ]$nodedepth,
-                        nsplit    = hyp[i, ]$nsplit,
-                        splitrule = hyp[i, ]$splitrule,
-                        cindex    = perf$cindex,
-                        brier     = perf$brier,
-                        auc       = perf$auc)
-  
-  res           <- rbind(res, new_row)
-  rownames(res) <- NULL
-}
-stop <- Sys.time()
---
+++ train_rsf_subset.R
@@ -0,0 +1,28 @@
+train_rsf_subset <- function(task, start_id, end_id) {
+  hyp <- expand.grid(
+    ntree     = seq(100, 2000, 200),
+    mtry      = seq(1:10),
+    nodesize  = seq(5, 100, 10),
+    nodedepth = seq(5, 100, 10),
+    nsplit    = seq(5, 15, 1),
+    splitrule = c("logrank", "logrankscore", "bs.gradient"))
+  
+  res_list <- lapply(start_id:end_id, function(i) {
+    perf <- train_rsf_model(task, hyp[i, ])
+    
+    data.frame(data      = "FD001",
+               ntree     = hyp[i, ]$ntree,
+               mtry      = hyp[i, ]$mtry,
+               nodesize  = hyp[i, ]$nodesize,
+               nodedepth = hyp[i, ]$nodedepth,
+               nsplit    = hyp[i, ]$nsplit,
+               splitrule = hyp[i, ]$splitrule,
+               cindex    = perf$cindex,
+               brier     = perf$brier,
+               auc       = perf$auc)
+  })
+  
+  res <- do.call(rbind, res_list)
+  rownames(res) <- NULL
+  write.csv(res, paste0("FD001_id_", start_id, "_", end_id, ".csv"), row.names = FALSE)
+}
