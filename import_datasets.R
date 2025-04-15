16d632a91b1db3ddb9abedc90ff2ddf196fd3cb4
Yiğitcan YARDIMCI <147248981+Yigitcanyardimci@users.noreply.github.com>
2025-04-15 10:09:34 +0300

    Update import_datasets.R

--- import_datasets.R
+++ import_datasets.R
@@ -3,4 +3,4 @@ train_FD002 <- read_csv("train_FD002.csv")
 train_FD003 <- read_csv("train_FD003.csv")
 train_FD004 <- read_csv("train_FD004.csv")
 
-FD001_all_results <- read_csv("~/Desktop/FD001_all_results.csv")
+FD001_all_results <- read_csv("FD001_all_results.csv")
