db48c138607ff2874bcb1d1df6e882310cd7f146
Yiğitcan YARDIMCI <147248981+Yigitcanyardimci@users.noreply.github.com>
2025-04-17 02:10:15 +0300

    Update run.R

--- run.R
+++ run.R
@@ -3,9 +3,11 @@ source("import_datasets.R")
 source("task.R")
 source("modeling.R")
 source("train_rsf_subset.R")
+source("tunability_overall.R")
+source("tunability_params.R")
 
-task_FD001 <- make_task(data      = train_FD001,
-                        censor    = 250,
+task_FD001 <- make_task(data = train_FD001,
+                        censor = 250,
                         variables = c("unit_ID", 
                                       "RUL",
                                       "operational_setting_3",
@@ -17,50 +19,22 @@ task_FD001 <- make_task(data      = train_FD001,
                                       "sensor_19",
                                       "sensor_5"))
 
-# örnek: train_rsf_subset(task_FD001, 1, 10)
-
-
-
 default_params <- list(
   ntree      = 500,
   mtry       = 3,
   nodesize   = 15,
-  nodedepth  = 100,
+  nodedepth  = 15,
   nsplit     = 10,
   splitrule  = "logrank"
 )
+
 default_perf <- train_rsf_model(task_FD001, default_params)
 
-FD001_all_results <- read_csv("FD001_all_results.csv")
+FD001_all_results <- read_csv("~/Desktop/FD001_all_results.csv")
 
-best_cindex <- FD001_all_results %>%
-  filter(!is.na(cindex)) %>%
-  summarise(best = max(cindex)) %>%
-  pull(best)
+overall_summary <- tune_overall(default_perf, FD001_all_results, subset_name = "FD001")
+print(overall_summary, row.names = FALSE)
 
-best_brier <- FD001_all_results %>%
-  filter(!is.na(brier)) %>%
-  summarise(best = min(brier)) %>% 
-  pull(best)
-
-tunability_cindex <- round(best_cindex - default_perf$cindex, 4)
-tunability_brier <- round(default_perf$brier - best_brier, 4)
-
-tunability_summary <- data.frame(
-  subset              = "FD001",
-  default_cindex      = round(default_perf$cindex, 4),
-  best_cindex         = round(best_cindex, 4),
-  tunability_cindex   = tunability_cindex,
-  default_brier       = round(default_perf$brier, 4),
-  best_brier          = round(best_brier, 4),
-  tunability_brier    = tunability_brier
-)
-
-print(tunability_summary)
-
-
-
-
-
-
+parameter_summary <- tune_params(default_perf, FD001_all_results, subset_name = "FD001")
+print(parameter_summary, row.names = FALSE)
 
