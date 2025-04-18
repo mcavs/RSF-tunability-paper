d72f12fbbd126f9419d21da94691530a76a544eb
Yiğitcan YARDIMCI <147248981+Yigitcanyardimci@users.noreply.github.com>
2025-04-19 00:22:50 +0300

    Update import_datasets.R

--- import_datasets.R
+++ import_datasets.R
@@ -4,3 +4,13 @@ train_FD003 <- read_csv("train_FD003.csv")
 train_FD004 <- read_csv("train_FD004.csv")
 
 FD001_all_results <- read_csv("FD001_all_results.csv")
+FD002_all_results <- read_csv("FD002_all_results.csv")
+FD003_all_results <- read_csv("FD003_all_results.csv")
+FD004_all_results <- read_csv("FD004_all_results.csv")
+
+all_results <- bind_rows(FD001_all_results,
+                         FD002_all_results,
+                         FD003_all_results,
+                         FD004_all_results)
+
+write_csv(all_results, "all_results.csv")
